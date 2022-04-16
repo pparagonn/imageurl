09ecf391c049e9682f2e5850ad2809039b1ec07f
Jovany Melchor <jmelchor@hawk.iit.edu>
2022-04-16 14:17:18 -0500

    Add html parser from stencil

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from bs4 import BeautifulSoup
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def getdata(url):
+    r = requests.get(url)
+    return r.text
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+htmldata = getdata("https://www.geeksforgeeks.org/")
+soup = BeautifulSoup(htmldata, 'html.parser')
+for item in soup.find_all('img'):
+    print(item['src'])
